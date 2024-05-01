68dcd069c96f526e4a61ae735029f6f836591d2b
Baruc1996 <jonatan.borges2012@hotmail.com>
2024-04-30 22:14:03 -0300

    Envio do projeto e-commerce, ainda em construção

---
+++ pedido/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from django.views import View
+from django.http import HttpResponse
+
+class Pagar(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Pagar')
+
+class SalvarPedido(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Fechar pedido')
+
+class Detalhe(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Detalhe')
+
